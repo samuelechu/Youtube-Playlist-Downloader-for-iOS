c54f5bf3df3b93197bad744dc213582a4105319a
samuelechu <samuelechu@gmail.com>
2015-07-03 12:59:45 -0700

    Initial Commit

---
+++ Music PlayerTests/Music_PlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Music_PlayerTests.swift
+//  Music PlayerTests
+//
+//  Created by Sem on 7/3/15.
+//  Copyright (c) 2015 Sem. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Music_PlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
