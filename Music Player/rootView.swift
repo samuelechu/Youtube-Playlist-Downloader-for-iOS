c49e4b8f27910341e4c282f2b97e4ac709cfd5ab
samuelechu <samuelechu@gmail.com>
2015-07-03 21:51:35 -0700

    .

---
+++ Music Player/rootView.swift
@@ -0,0 +1,39 @@
+//
+//  rootView.swift
+//  Music Player
+//
+//  Created by Sem on 7/3/15.
+//  Copyright (c) 2015 Sem. All rights reserved.
+//
+
+import UIKit
+
+class rootView: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for viewController in self.viewControllers!
+        {
+            viewController.view;
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
